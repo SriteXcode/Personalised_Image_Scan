0d481c05b33736fd6735669a40ffe34e842cc867
SriteXcode <riteshgangster420@gmail.com>
2025-09-08 12:27:57 +0530

    add backend link to server.js

--- client/Socket.jsx
+++ client/Socket.jsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_BACKEND_URL; // change if deployed  
-const socket = io(SOCKET_URL, {
+const socket = io("https://personalised-image-scan.onrender.com", {
   transports: ["websocket", "polling"],
 });
 
