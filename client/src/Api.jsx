dd8e3f0917da907a0f9452742596f18266896cf9
SriteXcode <riteshgangster420@gmail.com>
2025-09-08 11:55:02 +0530

    added backend link

--- client/src/Api.jsx
+++ client/src/Api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://personalised-image-scan.onrender.com/api",
+  baseURL: "https://personalised-image-scan-1.onrender.com/api",
 });
 
 // Add token if exists
