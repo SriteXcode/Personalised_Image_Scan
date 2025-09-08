9221f466a8e606b363e4be777479dc1f03db3ae7
SriteXcode <riteshgangster420@gmail.com>
2025-09-08 11:53:55 +0530

    added backend link

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
     e.preventDefault();
     try {
       await API.post("/auth/register", { name, email, password });
-      navigate("/login");
+      navigate("/");
     } catch (err) {
       setError(err.response?.data?.message || "Registration failed");
     }
